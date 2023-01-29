26ae7d7a1709e8eca582c5f657babc8cf057953f
Abdullah Al Faroque <rfnshare@gmail.com>
2023-01-29 19:59:48 +0600

    load json from a file

--- json parsing/jsonParsing.py
+++ json parsing/jsonParsing.py
@@ -4,3 +4,10 @@ courses = '{"name": "Faroque", "languages": ["C++", "Python"]}'
 
 data = json.loads(courses)
 print(data['languages'][1])
+
+print("************************")
+# open json file from path
+with open("sample.json", 'r') as f:
+    data = json.load(f)
+
+print(data['glossary']['GlossDiv']['GlossList']['GlossEntry']['GlossDef']['GlossSeeAlso'][0])
