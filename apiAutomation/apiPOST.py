b82ce5b80a780836e9adb5632795f3b21d1aa303
Abdullah Al Faroque <rfnshare@gmail.com>
2023-01-31 12:12:59 +0600

    Add a book & delete it using post method

--- apiAutomation/apiPOST.py
+++ apiAutomation/apiPOST.py
@@ -1,13 +1,27 @@
+import secrets
+import string
+from time import sleep
+
 import requests
 
+id = res = ''.join(secrets.choice(string.ascii_uppercase + string.digits)
+                   for i in range(7))
 response = requests.post("http://216.10.245.166/Library/Addbook.php",
                          json=
                          {
                              "name": "Learn Appium Automation with Java",
-                             "isbn": "bcd234872",
+                             "isbn": id,
                              "aisle": "227",
                              "author": "John foe"
-                         }, headers={"Content-Type": "application/json"},)
+                         }, headers={"Content-Type": "application/json"}, )
 
-print(response.status_code)
-print(response.json()['ID'])
+print(response.json())
+
+
+resp = requests.post("http://216.10.245.166/Library/DeleteBook.php",
+                     json=
+                     {
+                         "ID": response.json()['ID']
+                     }, headers={"Content-Type": "application/json"}, )
+print(resp.json())
+assert resp.status_code == 200
