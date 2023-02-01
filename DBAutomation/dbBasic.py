a09d083f8534ed488568f768e853891cf684075d
Abdullah Al Faroque <rfnshare@gmail.com>
2023-02-01 21:27:59 +0600

    Basic DB Fetching & Calculate

--- DBAutomation/dbBasic.py
+++ DBAutomation/dbBasic.py
@@ -1,5 +1,16 @@
 import mysql.connector
 
 # host, db, user, password
-conn = mysql.connector.connect(host='localhost', database='PythonAutomation', user='root', password='p@ssw0rd')
+conn = mysql.connector.connect(host='localhost', database='APIDevelop', user='root', password='p@ssw0rd')
 print(conn.is_connected())
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM CustomerInfo')
+# row = cursor.fetchone()
+# print(row[3])
+row = cursor.fetchall()
+print(len(row))
+add = 0
+for i in row:
+    add += i[2]
+print(add)
+conn.close()
