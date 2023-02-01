02607b3a9c4f099c42b8b7510b35adba538f06c3
Abdullah Al Faroque <rfnshare@gmail.com>
2023-02-01 21:46:19 +0600

    Update & Delete using cursor execute

--- DBAutomation/dbBasic.py
+++ DBAutomation/dbBasic.py
@@ -4,6 +4,8 @@ import mysql.connector
 conn = mysql.connector.connect(host='localhost', database='APIDevelop', user='root', password='p@ssw0rd')
 print(conn.is_connected())
 cursor = conn.cursor()
+
+# Read
 cursor.execute('SELECT * FROM CustomerInfo')
 # row = cursor.fetchone()
 # print(row[3])
@@ -13,4 +15,16 @@ add = 0
 for i in row:
     add += i[2]
 print(add)
+
+# Update
+query = "UPDATE CustomerInfo SET Location = %s WHERE CourseName = %s"
+data = ("BD", "Jmeter")
+cursor.execute(query, data)
+conn.commit()
+
+# Delete
+query2 = "DELETE FROM CustomerInfo WHERE CourseName = %s"
+data = ["Appium"]
+cursor.execute(query2, data)
+conn.commit()
 conn.close()
