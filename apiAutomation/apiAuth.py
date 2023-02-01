32a0777e881b4190cadb174835feded68115ad43
Abdullah Al Faroque <rfnshare@gmail.com>
2023-02-01 11:51:38 +0600

    auth using session manager

--- apiAutomation/apiAuth.py
+++ apiAutomation/apiAuth.py
@@ -1,5 +1,12 @@
 import requests
+from utilities.secrects import Info
 
-res = requests.get('https://api.github.com/users/rfnshare', verify=True, auth=('user', 'pass'))
+se = requests.session()
+se.auth = auth = ('rfnshare', Info.token)
+
+res = se.get('https://api.github.com/user')
 print(res.status_code)
 print(res.json()['company'])
+
+res2 = se.get('https://api.github.com/user/repos')
+print(res2.status_code)
