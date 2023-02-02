e6235c8db8a5db3eae6dc7b6148622a59ba55e76
Abdullah Al Faroque <rfnshare@gmail.com>
2023-02-02 12:02:31 +0600

    using kargs on requests post call

--- apiAutomation/apiPOST.py
+++ apiAutomation/apiPOST.py
@@ -24,14 +24,22 @@ book_id = ''.join(secrets.choice(string.ascii_uppercase + string.digits)
 
 # Create Book using DB
 query = 'SELECT * FROM Books'
-response = requests.post(config["API"]["host"] + APIResources.addBook, json=addBookPayloadFromDB(query), headers=headers,)
-print(response.text)
-print(response.status_code)
+create_data = {
+    'url': config["API"]["host"] + APIResources.addBook,
+    'json': addBookPayloadFromDB(query),
+    'headers': headers
+}
+response = requests.post(**create_data)
 assert "successfully added" in response.json()['Msg']
+created_id = response.json()['ID']
 
 # Delete Book
-created_id = response.json()['ID']
-resp = requests.post(config["API"]["host"] + APIResources.deleteBook, json=deleteBookPayload(created_id), headers=headers,)
+delete_data = {
+    'url': config["API"]["host"] + APIResources.deleteBook,
+    'json': deleteBookPayload(created_id),
+    'headers': headers
+}
+resp = requests.post(**delete_data)
 assert resp.status_code == 200
 assert "successfully deleted" in resp.json()['msg']
 
