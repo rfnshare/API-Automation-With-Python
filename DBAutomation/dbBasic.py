c9b255de38dd9ccbfa81de9446fac264fefcab50
Abdullah Al Faroque <rfnshare@gmail.com>
2023-02-02 10:43:01 +0600

    Organize DB properties & data

--- DBAutomation/dbBasic.py
+++ DBAutomation/dbBasic.py
@@ -1,8 +1,7 @@
-import mysql.connector
+from utilities.config import getConnection
 
-# host, db, user, password
-conn = mysql.connector.connect(host='localhost', database='APIDevelop', user='root', password='p@ssw0rd')
-print(conn.is_connected())
+# Create a connection
+conn = getConnection()
 cursor = conn.cursor()
 
 # Read
@@ -14,17 +13,26 @@ print(len(row))
 add = 0
 for i in row:
     add += i[2]
-print(add)
+    print(i)
+# assert add == 121
+print("Total Amount", add)
 
 # Update
-query = "UPDATE CustomerInfo SET Location = %s WHERE CourseName = %s"
-data = ("BD", "Jmeter")
-cursor.execute(query, data)
-conn.commit()
+# query = "UPDATE CustomerInfo SET Amount = %s WHERE CourseName = %s"
+# data = (99, "Appium")
+# cursor.execute(query, data)
+# conn.commit()
 
 # Delete
-query2 = "DELETE FROM CustomerInfo WHERE CourseName = %s"
-data = ["Appium"]
-cursor.execute(query2, data)
-conn.commit()
-conn.close()
+# query2 = "DELETE FROM CustomerInfo WHERE CourseName = %s"
+# data = ["Appium"]
+# cursor.execute(query2, data)
+# conn.commit()
+# conn.close()
+
+# Create
+# query3 = "INSERT INTO CustomerInfo values(%s,CURRENT_DATE(),21,%s)"
+# data = ("Appium", "Germany")
+# cursor.execute(query3, data)
+# conn.commit()
+# conn.close()
--- utilities/config.py
+++ utilities/config.py
@@ -1,7 +1,29 @@
 import configparser
+from mysql.connector import Error
+
+import mysql.connector
 
 
 def getConfig():
     config = configparser.ConfigParser()
     config.read('../utilities/properties.ini')
     return config
+
+
+# host, db, user, password
+connect_config = {
+    'user': getConfig()['SQL']['user'],
+    'password': getConfig()['SQL']['password'],
+    'host': getConfig()['SQL']['host'],
+    'database': getConfig()['SQL']['database']
+}
+
+
+def getConnection():
+    try:
+        conn = mysql.connector.connect(**connect_config)
+        if conn.is_connected():
+            print("Connected")
+            return conn
+    except Error as e:
+        print("Error->", e)
