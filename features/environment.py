3e5b00af4f89c988342c4ca900b3605f27c65ee3
Abdullah Al Faroque <rfnshare@gmail.com>
2023-02-02 21:05:52 +0600

    add environment flavor for automated delete book

---
+++ features/environment.py
@@ -0,0 +1,20 @@
+import requests
+
+from apiAutomation.payload import deleteBookPayload
+from utilities.config import getConfig
+from utilities.resources import APIResources
+
+config = getConfig()
+headers = {"Content-Type": "application/json"}
+
+
+def before_scenario(context, scenario):
+    print("Welcome To Hogwarts")
+
+
+def after_scenario(context, scenario):
+    resp = requests.post(config["API"]["host"] + APIResources.deleteBook, json=deleteBookPayload(context.book_id),
+                         headers=headers, )
+    print("Deleted:", context.book_id)
+    assert resp.status_code == 200
+    assert "successfully deleted" in resp.json()['msg']
