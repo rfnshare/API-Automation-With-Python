fcc569a2da3acb67657aa3a3dc30d34658c1880b
Abdullah Al Faroque <aalfaroque@gmail.com>
2023-09-06 11:40:59 +0600

    Revising API Auth

--- apiAutomation/apiAuth.py
+++ apiAutomation/apiAuth.py
@@ -7,6 +7,7 @@ se.auth = auth = ('rfnshare', Info.token)
 res = se.get('https://api.github.com/user')
 print(res.status_code)
 print(res.json()['company'])
-
+for i, j in (res.json()).items():
+    print(i, j)
 res2 = se.get('https://api.github.com/user/repos')
 print(res2.status_code)
